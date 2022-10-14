bca7d3fd40d1ff39b5d91657e8f56b84640bb0ba
Sergei <mail@mail.ru>
2022-10-15 00:05:28 +0500

    exercise

---
+++ Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] stringArray1 = { "he", "llo", "world", "-2", "123" };
+string[] stringArray2 = new string[5];
+
+GetStringArray(stringArray1);
+System.Console.WriteLine(string.Join(" ", stringArray2));
+
+
+
+void GetStringArray(string[] str)
+{
+    int count = 0;
+    for (int i = 0; i < stringArray1.Length; i++)
+    {
+        if (stringArray1[i].Length < 4)
+        {
+            stringArray2[count] = stringArray1[i];
+            count++;
+        }
+    }
+}
